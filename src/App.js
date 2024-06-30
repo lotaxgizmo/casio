79a527abb562e8c1dfca33a5a83b5b95f31920ff
lotaxgizmo <lotaoko2@gmail.com>
2024-06-30 05:49:25 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import banner from "./assets/casio.jpg"
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="flex flex-col justify-center items-center text-center p-4">
+      <img src={banner} alt="" />
+      <h1 className="text-5xl font-black">CASIO COIN  WEBSITE</h1>
+      <h1 className="text-5xl font-black">coming soon</h1>
     </div>
   );
 }
